598e3f96324a4f3a39cca1994a06b55a9e50d7c0
ClothingStorefirst <zohangames9@gmail.com>
2025-06-03 22:00:29 -0500

    version_3.2

--- demo/src/main/java/com/example/demo/LoginController.java
+++ demo/src/main/java/com/example/demo/LoginController.java
@@ -63,6 +63,7 @@ public class LoginController {
             logger.info("Inicio de sesión exitoso como ADMIN: {}", email);
             return "redirect:/admin";
         }
+         // Apache Commons: validación de email asdasdasdsa
 
         Conductor conductor = conductorDAO.obtenerPorCorreo(email);
         Optional<Conductor> optConductor = Optional.fromNullable(conductor); // Guava: Optional para evitar null
